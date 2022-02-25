0959867655bc8f3f7209dc2ca043ca6208be78c5
Joaquín Mellado Quevedo <mellado.joaquin@gmail.com>
2022-02-24 21:41:03 -0300

    [feat]: game play

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,47 @@
 module Main exposing (..)
 
-import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (..)
+import Html.Events exposing (onClick)
+import Json.Decode exposing (..)
+import Set exposing (Set)
+import Http exposing (..)
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { guesses : Set String
+    , gameState : GameState
+    }
+
+
+type GameState
+    = Loading
+    | Running String
+    | Won
+    | Lost
+    | Error String
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { guesses = Set.empty
+      , gameState = Loading
+      }
+    , fetchWord
+    )
+
+
+fetchWord : Cmd Msg
+fetchWord =
+    Http.send NewPhrase <|
+        Http.get "https://snapdragon-fox.glitch.me/word" wordDecoder
+
+
+wordDecoder : Json.Decode.Decoder String
+wordDecoder =
+    Json.Decode.field "word" Json.Decode.string
 
 
 
@@ -22,12 +49,25 @@ init =
 
 
 type Msg
-    = NoOp
+    = Guess String
+    | Restart
+    | NewPhrase (Result Http.Error String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        Guess letter ->
+            ( { model | guesses = Set.insert letter model.guesses }, Cmd.none )
+
+        Restart ->
+            ( { model | guesses = Set.empty, gameState = Loading }, fetchWord )
+
+        NewPhrase (Ok phrase) ->
+            ( { model | gameState = Running phrase }, Cmd.none )
+
+        NewPhrase (Err error) ->
+            ( { model | gameState = Error <| toString error }, Cmd.none )
 
 
 
@@ -36,21 +76,85 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        ]
+    let
+        phraseHtml =
+            case model.gameState of
+                Loading ->
+                    span [] [ text "Loading" ]
+
+                Running phrase ->
+                    phrase
+                        |> String.split ""
+                        |> List.map
+                            (\letter ->
+                                if letter == " " then
+                                    " "
+                                else if Set.member letter model.guesses then
+                                    letter
+                                else
+                                    "_"
+                            )
+                        |> List.map
+                            (\entry ->
+                                span [] [ text entry ]
+                            )
+                        |> div []
+
+                Won ->
+                    span [] [ text "Congrats!" ]
+
+                Lost ->
+                    span [] [ text "Ooooooops" ]
+
+                Error error ->
+                    span [] [ text error ]
+
+        failuresHtml =
+            case model.gameState of
+                Running phrase ->
+                    model.guesses
+                        |> Set.toList
+                        |> List.filter
+                            (\letter ->
+                                not <| String.contains letter phrase
+                            )
+                        |> List.map
+                            (\letter ->
+                                span [] [ text letter ]
+                            )
+                        |> div []
+
+                _ ->
+                    text ""
+
+        buttonsHtml =
+            "abcdefghijklmnopqrstuvwxyz"
+                |> String.split ""
+                |> List.map
+                    (\letter ->
+                        button [ onClick <| Guess letter ] [ text letter ]
+                    )
+                |> div []
+    in
+        div []
+            [ phraseHtml
+            , buttonsHtml
+            , failuresHtml
+            , div []
+                [ button [ onClick Restart ] [ text "Restart" ]
+                ]
+            ]
 
 
 
 ---- PROGRAM ----
 
 
-main : Program () Model Msg
+main : Program Never Model Msg
 main =
-    Browser.element
+    Html.program
         { view = view
-        , init = \_ -> init
+        , init = init
         , update = update
         , subscriptions = always Sub.none
         }
